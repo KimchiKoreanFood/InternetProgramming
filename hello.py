ab570bb493f984c9a889ce39cfb2e5fa5dacb104
koreakimchi <s60162114@classnet.mju.ac.kr>
2021-05-14 15:56:31 +0900

    add my name

--- hello.py
+++ hello.py
@@ -5,8 +5,14 @@ def fun_2():
     print(" World")
     
  def fun_3():
-    print(" GitHub")   
+    print(" GitHub") 
+
+
+def fun_4():
+    print("\n I M JAEJUN");
+
 
 fun_1()
 fun_2()
 fun_3()
+fun_4()
